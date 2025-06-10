21dc7ec117f7528179e0b672157350bf04d67226
Jaret <gadir041499@gmail.com>
2025-06-10 00:31:04 -0600

    Añadir funcionalidad para gestionar retos personales: implementar componentes para agregar y listar retos, y crear API para manejar datos de usuario.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,10 @@
+/**
+ * El código comentado es un ejemplo de middleware en Astro que maneja la localización
+ * basado en el encabezado "Accept-Language". Dependiendo del idioma, se establece un título
+ * diferente en el contexto local. Este middleware se ejecuta antes de procesar la solicitud.
+ * Pero con la implementación actual de Clerk, no es necesario manejar la localización manualmente.
+ * Si se desea implementar un middleware personalizado, se puede descomentar y ajustar según sea necesario.
+ */
 // import { defineMiddleware } from "astro:middleware";
 
 // export const onRequest = defineMiddleware((context, next ) => {
